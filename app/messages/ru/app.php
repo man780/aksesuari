2dcbffe17887b1fb74871495713b943d0730d48b
Murod <man780@mail.ru>
2017-02-23 18:57:51 +0500

    Первый коммит

---
+++ app/messages/ru/app.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 24
+ * Date: 23.02.2017
+ * Time: 18:19
+ */
+return [
+    'Download price list' => 'Скачать Прайс лист',
+    'Last items' => 'Последние просмотры',
+    'Home' => 'Домой',
+    'Shop' => 'Магазин',
+    'News' => 'Новости',
+    'Gallery' => 'Галерея',
+    'Guestbook' => 'Обращения',
+    'FAQ' => 'Частые вопросы',
+    'Contact' => 'Контакты',
+    'empty' => 'пусто',
+    'Administrator' => 'Администратор',
+    'Message successfully added' => 'Сообщение успешно доставлено',
+    '' => '',
+];
---
+++ app/views/layouts/main.php
@@ -0,0 +1,82 @@
+<?php
+use yii\easyii\modules\shopcart\api\Shopcart;
+use yii\easyii\modules\subscribe\api\Subscribe;
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
+use yii\widgets\Menu;
+
+$goodsCount = count(Shopcart::goods());
+?>
+<?php $this->beginContent('@app/views/layouts/base.php'); ?>
+<div id="wrapper" class="container">
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="<?= Url::home() ?>">Akssesuari.uz</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <?= Menu::widget([
+                        'options' => ['class' => 'nav navbar-nav'],
+                        'items' => [
+                            //['label' => Yii::t('app', 'Home'), 'url' => ['site/index']],
+                            ['label' => Yii::t('app', 'Shop'), 'url' => ['shop/index']],
+                            ['label' => Yii::t('app', 'News'), 'url' => ['news/index']],
+                            //['label' => Yii::t('app', 'Articles'), 'url' => ['articles/index']],
+                            //['label' => Yii::t('app', 'Gallery'), 'url' => ['gallery/index']],
+                            ['label' => Yii::t('app', 'Guestbook'), 'url' => ['guestbook/index']],
+                            ['label' => Yii::t('app', 'FAQ'), 'url' => ['faq/index']],
+                            ['label' => Yii::t('app', 'Contact'), 'url' => ['/contact/index']],
+                        ],
+                    ]); ?>
+                    <!--<a href="<?/*= Url::to(['/shopcart']) */?>" class="btn btn-default navbar-btn navbar-right" title="Complete order">
+                        <i class="glyphicon glyphicon-shopping-cart"></i>
+                        <?php /*if($goodsCount > 0) : */?>
+                            <?/*= $goodsCount */?> <?/*= $goodsCount > 1 ? 'items' : 'item' */?> - <?/*= Shopcart::cost() */?>$
+                        <?php /*else : */?>
+                            <span class="text-muted"><?/*=Yii::t('app', 'empty')*/?></span>
+                        <?php /*endif; */?>
+                    </a>-->
+
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <?php if($this->context->id != 'site') : ?>
+            <br/>
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ])?>
+        <?php endif; ?>
+        <?= $content ?>
+        <div class="push"></div>
+    </main>
+</div>
+<footer>
+    <div class="container footer-content">
+        <div class="row">
+            <div class="col-md-2">
+                Subscribe to newsletters
+            </div>
+            <div class="col-md-6">
+                <?php if(Yii::$app->request->get(Subscribe::SENT_VAR)) : ?>
+                    You have successfully subscribed
+                <?php else : ?>
+                    <?= Subscribe::form() ?>
+                <?php endif; ?>
+            </div>
+            <div class="col-md-4 text-right">
+                ©2015 noumo
+            </div>
+        </div>
+    </div>
+</footer>
+<?php $this->endContent(); ?>
